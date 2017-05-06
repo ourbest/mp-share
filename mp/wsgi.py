07a7644b20a3987f32d11e32976ba3ae325aa3b0
ourbest <chenyonghui@gmail.com>
2017-05-06 17:29:26 +0800

    use pymysql

--- mp/wsgi.py
+++ mp/wsgi.py
@@ -11,6 +11,10 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+import pymysql
+
+pymysql.install_as_MySQLdb()
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mp.settings")
 
 application = get_wsgi_application()
