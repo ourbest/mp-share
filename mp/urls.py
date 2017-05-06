71988362f9ffa6ddf159086a266eafa1bac86b0e
ourbest <chenyonghui@gmail.com>
2017-05-06 17:09:56 +0800

    add static

--- mp/urls.py
+++ mp/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 import api.urls
 
@@ -22,3 +23,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include(api.urls)),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
