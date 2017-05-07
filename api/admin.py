f56fa05e1ea4ffe9abf6851a74ccdfadee2accf9
ourbest <chenyonghui@gmail.com>
2017-05-07 22:40:29 +0800

    add search and filter

--- api/admin.py
+++ api/admin.py
@@ -3,6 +3,8 @@ from django.utils.html import format_html
 
 from api.models import *
 
+admin.site.disable_action('delete_selected')
+
 
 # Register your models here.
 
@@ -18,6 +20,7 @@ class WxURLAdmin(admin.ModelAdmin):
     _get_image.short_description = '图片'
     _get_image.allow_tags = True
     ordering = ('-id',)
+    search_fields = ('title',)
 
 
 @admin.register(WxShareURL)
@@ -26,7 +29,8 @@ class WxShareURLAdmin(admin.ModelAdmin):
     ordering = ('-id',)
 
     def _get_title(self, obj):
-        return format_html('<a href="{url}" width="80" target="_blank">{title}</a>', url=obj.wx_url.url, title=obj.wx_url.title)
+        return format_html('<a href="{url}" width="80" target="_blank">{title}</a>', url=obj.wx_url.url,
+                           title=obj.wx_url.title)
 
     _get_title.short_description = '文章标题'
 
@@ -34,12 +38,15 @@ class WxShareURLAdmin(admin.ModelAdmin):
         return WxClick.objects.filter(wx_share_url=obj).values_list('uuid', flat=True).distinct().count()
 
     _get_uv.short_description = 'UV'
+    list_filter = ('wx_user__name',)
+    search_fields = ('wx_url__title',)
 
 
 @admin.register(WxClick)
 class WxClickAdmin(admin.ModelAdmin):
     list_display = ('wx_share_url', 'ip', 'ua', 'created_at')
     ordering = ('-id',)
+    list_filter = ('wx_share_url__wx_user__name',)
 
     # def title(self, obj):
     #     return str(obj.wx_share_url)
